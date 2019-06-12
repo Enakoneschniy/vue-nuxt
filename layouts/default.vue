695f895081badbc113a067b9f90771dec6f0f1a0
Eugene Nakoneschniy <evgeniy.nakoneschniy@suttermills.com>
2019-06-12 19:16:58 +0300

    add new files

---
+++ layouts/default.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <ul>
+      <li>
+        <nuxt-link to="/" exact>Home</nuxt-link>
+      </li>
+      <li>
+        <nuxt-link :to="{name: 'about'}" tag="button" exact>About</nuxt-link>
+      </li>
+      <li>
+        <nuxt-link :to="{name: 'contacts'}" tag="button" exact>Contacts</nuxt-link>
+      </li>
+      <li>
+        <nuxt-link :to="{ name: 'users' }" tag="button" exact>User 123</nuxt-link>
+      </li><li>
+        <nuxt-link :to="{ name: 'users-detail', params: { id: '123' } }" tag="button" exact>User 123</nuxt-link>
+      </li>
+    </ul>
+    <nuxt/>
+  </div>
+</template>
+
+<style scoped lang="sass">
+  ul
+    padding: 0
+    margin: 10px 0 0
+    list-style: none
+    display: flex
+    justify-content: space-around
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <section class="container">
+    <div>
+      <logo/>
+      <ul>
+        <li>
+          <nuxt-link :to="{name: 'tab-1'}">Tab 1</nuxt-link>
+        </li>
+        <li>
+          <nuxt-link :to="{name: 'tab-2'}">Tab 2</nuxt-link>
+        </li>
+      </ul>
+      <div class="child-container">
+        <nuxt-child/>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+  import Logo from '../components/Logo.vue'
+
+  export default {
+    components: {
+      Logo
+    }
+  }
+</script>
+
+<style scoped lang="sass">
+  .child-container
+    border: 1px solid gray
+    width: 80%
+    margin: 0 auto
+    min-height: 200px
+</style>
